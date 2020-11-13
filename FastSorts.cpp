a51b50b405aae989c45e368cfecdcbec9861c1d6
J-Herron <70306343+J-Herron@users.noreply.github.com>
2020-11-13 13:42:34 -0600

    Update FastSorts.cpp

--- FastSorts.cpp
+++ FastSorts.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 using namespace std;
 
+//Credit: Data Structures Sorts Slides (slide 38) (void split and void quicksort)
+//Credit: https://www.geeksforgeeks.org/cpp-program-for-quicksort/ (void swapQ)
+
+void swapQ(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
+void split(int arr[], int first, int last, int& position)
+{
+    int pivot = arr[first]; //pivot the element
+    int left = first; //index for left search
+    int right = last; //index for right search
+
+    while (left < right) 
+    {
+        while (pivot < arr[right]) //Searches from the right
+            right--;
+        while (left < right && (arr[left] < pivot || arr[left] == pivot)) //Search from the left
+            left++;
+
+        if (left < right) //If the searches haven't happened
+            swapQ(arr[left, arr[right]); //Interchange the elements
+    }
+
+    //End of searches, places pivot in the correct position
+    position = right;
+    arr[first] = arr[position]; 
+    arr[position] = pivot; 
+}
+
+void quickSort(int arr[],int first, int last)
+{
+    //if the first number is less than the last number 
+    if (first < last)
+    {
+        split(arr, first, last, position); //Split into 2
+        quickSort(arr, first, position - 1); //Sort left
+        quickSort(arr, position + 1, last); //Sort right 
+    }
+}
+
 //Credit to GeeksforGeeks 
 //See source: https://www.geeksforgeeks.org/heap-sort/?ref=lbp
 //Program to perform Heap Sort 
