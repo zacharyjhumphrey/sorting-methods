b25b59c8e1f3ea412c600aae7a4843c4b40c2572
J-Herron <70306343+J-Herron@users.noreply.github.com>
2020-11-15 17:12:31 -0600

    Update FastSorts.h

--- FastSorts.h
+++ FastSorts.h
@@ -3,6 +3,7 @@
 #define FASTSORTS_H
 
 void swapQ(int* a, int* b)
+void split(int arr[], int first, int last, int& position)
 void QuickSort(int arr[], int first, int last);
 void swapArray(int arr[], int arr2[]);
 void heapSort(int arr[], int n);
